7de55104c79926c2181bc3617c70f5dfe44f4bbb
slarkX <915064810@qq.com>
2024-03-07 10:55:39 +0800

    Update DecoratorPattern.cpp

--- DecoratorPattern.cpp
+++ DecoratorPattern.cpp
@@ -56,16 +56,24 @@ public:
         // 添加额外的功能
         cout << "Added Functionality B" << endl;
     }
+    //新增业务方法
+    void addedBehavior() {
+        cout << "New method in Concrete Decorator B" << endl;
+    }
 };
 
 int main() {
+    //使用抽象构建类型定义
     Component* component = new ConcreteComponent();
     Component* decoratorA = new ConcreteDecoratorA(component);
     Component* decoratorB = new ConcreteDecoratorB(decoratorA);
-
     decoratorB->operation();
-    ConcreteDecoratorA* concreteDecoratorA = dynamic_cast<ConcreteDecoratorA*>(decoratorA);
+
+    //使用具体装饰类型定义
+    ConcreteDecoratorA* concreteDecoratorA = dynamic_cast<ConcreteDecoratorA*>(decoratorB);
     concreteDecoratorA->addedBehavior(); //调用装饰器A的新方法
+    ConcreteDecoratorB* concreteDecoratorB = dynamic_cast<ConcreteDecoratorB*>(concreteDecoratorA);
+    concreteDecoratorB->addedBehavior();
 
     delete decoratorB;
     delete decoratorA;
